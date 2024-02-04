c8d01356c7de6e90ab88aa518fa493d160c3877d
01Dri <drikill01@gmail.com>
2024-02-04 03:15:31 -0300

    Kafka cosumer and producer with spring implemented

---
+++ src/main/java/me/dri/LearningKafkaWithSpring/Consumer.java
@@ -0,0 +1,13 @@
+package me.dri.LearningKafkaWithSpring;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Consumer {
+
+    @KafkaListener(topics = "ecomerce.cliente2", groupId = "ecomerce-groupid")
+    public void consumer(String message) {
+        System.out.println(message);
+    }
+}
--- src/main/java/me/dri/LearningKafkaWithSpring/LearningKafkaWithSpringApplication.java
+++ src/main/java/me/dri/LearningKafkaWithSpring/LearningKafkaWithSpringApplication.java
@@ -2,8 +2,10 @@ package me.dri.LearningKafkaWithSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class LearningKafkaWithSpringApplication {
 
 	public static void main(String[] args) {
