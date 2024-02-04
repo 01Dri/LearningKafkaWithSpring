3c7221106d3ed9e079c479fdecab8bcbfdd72fd8
01Dri <drikill01@gmail.com>
2024-02-03 21:37:18 -0300

    Initialing spring project

---
+++ src/main/java/me/dri/LearningKafkaWithSpring/LearningKafkaWithSpringApplication.java
@@ -0,0 +1,13 @@
+package me.dri.LearningKafkaWithSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningKafkaWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningKafkaWithSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dri/LearningKafkaWithSpring/LearningKafkaWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dri.LearningKafkaWithSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningKafkaWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
